48291b912fe9bd6c4dfd75c5fbd6ac606c4b6ffc
Ethan Tolson <tol18009@byui.edu>
2023-01-08 10:18:05 -0700

    Added some blank files, updated readme

---
+++ constants.py
@@ -0,0 +1,5 @@
+from os import path
+FILEPATH = f"{path.dirname(path.abspath(__file__))}/"
+WIDTH = 100
+HEIGHT = 100
+TITLE = "Email"
---
+++ display/window.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+import math
+import constants
+
+class MainDisplay():
+    def __init__(self):
+        self.window = tk.TK()
+        self.window.title = "Email"
