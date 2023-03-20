06428357f95f41a2fb52f6c3e0ceecf5f9f4c5a3
EthanTolson <ethantolson1@gmail.com>
2023-03-20 12:38:14 -0600

    Added voice class and made a tester in the window

--- voice/test.py
+++ voice/test.py
@@ -2,6 +2,9 @@ from gtts import gTTS
 
 from os import system, path
 import speech_recognition as sr
+import sounddevice as sd
+from scipy.io.wavfile import write
+import numpy as np
 
 # from pydub import AudioSegment
 
@@ -9,21 +12,29 @@ import subprocess
 
 directory = f"{path.dirname(path.abspath(__file__))}/"
 filename = f"{directory}test3.mp3"
-wav = f"{directory}test3.wav"
+wav = f"{directory}test4.wav"
 
-test = "The red tiger ate the children.."
+# test = "The red tiger ate the children.."
 
 
-tts = gTTS(test)
+# tts = gTTS(test)
 
-tts.save(filename)
+# tts.save(filename)
 
-system(filename)
+# system(filename)
 
-subprocess.call(['ffmpeg', '-i', f'{filename.replace("//", "/")}', f'{wav.replace("//", "/")}'])
+# subprocess.call(['ffmpeg', '-i', f'{filename.replace("//", "/")}', f'{wav.replace("//", "/")}'])
 # AudioSegment.from_mp3("test1.mp3").export("test1.wav", format="wav")
 
 file = "test1.mp3"
+
+fs = 44100  # Sample rate
+seconds = 3  # Duration of recording
+print("______________Recording Beginning_____________________")
+myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1, dtype = np.int16)
+sd.wait()  # Wait until recording is finished
+print("______________Recording Finished______________________")
+write(wav, fs, myrecording.astype(np.int16))
 
 r = sr.Recognizer()
 with sr.AudioFile(wav) as source:
---
+++ voice/voice.py
@@ -0,0 +1,58 @@
+from gtts import gTTS
+from constants import FILEPATH
+from os import system, path, remove
+import speech_recognition as sr
+import sounddevice as sd
+from scipy.io.wavfile import write
+import numpy as np
+
+class VoiceControl:
+    def __init__(self):
+        self.speech_filename = f"{FILEPATH}temp.wav"
+        self.text_filename = f"{FILEPATH}temp.mp3"
+
+    def speech_to_text(self, length):
+        self.record_audio(length)
+        text = self.to_text()
+        self.delete_audio_file("wav")
+        return text
+    
+    def to_text(self):
+        try:
+            r = sr.Recognizer()
+            with sr.AudioFile(self.speech_filename) as source:
+                # listen for the data (load audio to memory)
+                audio_data = r.record(source)
+                # recognize (convert from speech to text)
+                text = r.recognize_google(audio_data)
+            return text
+        except:
+            print("Something went Wrong with speech to text.")
+            return ""
+
+    def record_audio(self, length):
+        fs = 44100  # Sample rate
+        seconds = length  # Duration of recording
+        print("______________Recording Beginning_____________________")
+        myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1, dtype = np.int16)
+        sd.wait()  # Wait until recording is finished
+        print("______________Recording Finished______________________")
+        write(self.speech_filename, fs, myrecording.astype(np.int16))
+
+    def delete_audio_file(self, type):
+        if type =="tts":
+            if path.exists(self.text_filename):
+                remove(self.text_filename)
+        else:
+            if path.exists(self.speech_filename):
+                remove(self.speech_filename)
+
+
+    def text_to_speech(self, text):
+        """
+        Gets an mp3 file from googles tts plays it then deletes it
+        """
+        tts = gTTS(text)
+        tts.save(self.text_filename)
+        system(self.text_filename)
+        self.delete_audio_file("tts")
